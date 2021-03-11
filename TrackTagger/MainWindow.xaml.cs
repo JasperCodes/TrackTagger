f053608d3606a592c02f026f620ddf482b1c2ab1
JasperCodes <jasper.timmermans@outlook.com>
2021-03-11 21:31:31 +0100

    add first working prototype

---
+++ TrackTagger/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace TrackTagger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private void CopyTagsButtonClick(object sender, RoutedEventArgs e)
+        {
+            List<string> selectedList = new();
+            foreach (var item in tagsListBox.SelectedItems)
+            {
+
+                string combinedString = "#" + item.ToString();
+
+                selectedList.Add(combinedString);
+            }
+
+            if (selectedList.Count == 0) { return; }
+
+            string joinedString = string.Join(", ", selectedList);
+
+            MessageBox.Show(joinedString);
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        
+    }
+}
